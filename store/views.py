1b0017d17baff6528e184f21ac98e49fa61082d6
chandra p <chandrap1705chan@gmail.com>
2026-09-26 20:15:42 +0530

    Check product count

--- store/views.py
+++ store/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 def home(request):
     products= Product.objects.all()
+    print("PRODUCT COUNT:", products.count())
     return render(request, 'home.html', {
         'products':products
         
